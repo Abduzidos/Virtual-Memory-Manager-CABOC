6442be3905944c7bff2b5b645c9ed650a887e461
OtacilioN <otacilio.n97@gmail.com>
2019-06-16 18:21:47 -0300

    Add debug levels to main.c

--- main.c
+++ main.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
+
+#define NO_DEBUG 0
+#define LOW 1
+#define MEDIUM 2
+#define HIGH 3
+#define DEBUG_LEVEL HIGH
+
+void debugHigh(const char *format, ...);
+void debugMedium(const char *format, ...);
+void debugLow(const char *format, ...);
 
 int main(void){
     FILE *addres_file = fopen("addresses.txt","rb");
@@ -8,6 +19,7 @@ int main(void){
 
     // Read all the address file
     while(fscanf(addres_file, "%d", &address[address_length]) != EOF){
+        debugHigh("Saved Address[%d] = %d\n", address_length, address[address_length]);
         address_length++;
         address = realloc(address, ((address_length+1)*sizeof(int)));
     }
@@ -15,4 +27,31 @@ int main(void){
 
     free(address);
     return(0);
+}
+
+void debugHigh(const char *format, ...)
+{
+    va_list arg;
+    va_start(arg, format);
+    if (DEBUG_LEVEL >= HIGH)
+        vfprintf(stdout, format, arg);
+    va_end(arg);
+}
+
+void debugMedium(const char *format, ...)
+{
+    va_list arg;
+    va_start(arg, format);
+    if (DEBUG_LEVEL >= MEDIUM)
+        vfprintf(stdout, format, arg);
+    va_end(arg);
+}
+
+void debugLow(const char *format, ...)
+{
+    va_list arg;
+    va_start(arg, format);
+    if (DEBUG_LEVEL >= LOW)
+        vfprintf(stdout, format, arg);
+    va_end(arg);
 }
